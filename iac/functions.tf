f930ecf37e241ec01deb81886b86c7fe692224f2
Lucas Jellema <lucasjellema@gmail.com>
2021-11-15 14:39:35 +0100

    iteration

--- iac/functions.tf
+++ iac/functions.tf
@@ -1,7 +1,8 @@
 
-resource "oci_artifacts_container_repository" "container_repository_functions" {
+resource "oci_artifacts_container_repository" "container_repository_functions_fakefun" {
+    # note: repository = store for all images versions of a specific container image - so it included the function name
     compartment_id = var.compartment_ocid
-    display_name = var.ocir_repo_name
+    display_name = "${var.ocir_repo_name}/${var.app_name}/fake-fun"
     is_immutable = false
     is_public = false
 }
@@ -14,7 +15,7 @@ resource "oci_functions_application" "cloudnative_2021_fn_app" {
 
 
 resource "null_resource" "Login2OCIR" {
-  depends_on = [oci_functions_application.cloudnative_2021_fn_app
+  depends_on = [oci_functions_application.cloudnative_2021_fn_app, oci_artifacts_container_repository.container_repository_functions_fakefun, 
     oci_identity_policy.faas_read_repos_tenancy_policy,
     oci_identity_policy.admin_manage_function_family_policy,
     oci_identity_dynamic_group.faas_dg,
@@ -26,7 +27,7 @@ resource "null_resource" "Login2OCIR" {
 }
 
 resource "null_resource" "FnPush2OCIR" {
-  depends_on = [null_resource.Login2OCIR, oci_functions_application.cloudnative_2021_fn_app]
+  depends_on = [null_resource.Login2OCIR, oci_functions_application.cloudnative_2021_fn_app, oci_artifacts_container_repository.container_repository_functions_fakefun]
 
   provisioner "local-exec" {
     command     = "image=$(docker images | grep ${local.app_name_lower} | awk -F ' ' '{print $3}') ; docker rmi -f $image &> /dev/null ; echo $image"
@@ -39,12 +40,12 @@ resource "null_resource" "FnPush2OCIR" {
   }
 
   provisioner "local-exec" {
-    command     = "image=$(docker images | grep fake-fun | awk -F ' ' '{print $3}') ; docker tag $image ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/fake-fun:${var.app_version}"
+    command     = "image=$(docker images | grep fake-fun | awk -F ' ' '{print $3}') ; docker tag $image ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/${var.app_name}/fake-fun:${var.app_version}"
     working_dir = "../functions/fake-fun"
   }
 
   provisioner "local-exec" {
-    command     = "docker push ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/fake-fun:${var.app_version}"
+    command     = "docker push ${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/${var.app_name}/fake-fun:${var.app_version}"
     working_dir = "../functions/fake-fun"
   }
 
@@ -54,7 +55,7 @@ resource "oci_functions_function" "tweet_retriever_fn" {
   depends_on     = [null_resource.FnPush2OCIR]
   application_id = oci_functions_application.cloudnative_2021_fn_app.id
   display_name   = "tweet_retriever"
-  image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/fake-fun:${var.app_version}"
+  image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/${var.app_name}/fake-fun:${var.app_version}"
   memory_in_mbs  = "256"
 }
 
@@ -62,7 +63,7 @@ resource "oci_functions_function" "tweet_summarizer_fn" {
   depends_on     = [null_resource.FnPush2OCIR]
   application_id = oci_functions_application.cloudnative_2021_fn_app.id
   display_name   = "tweet_summarizer"
-  image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/fake-fun:${var.app_version}"
+  image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${var.ocir_repo_name}/${var.app_name}/fake-fun:${var.app_version}"
   memory_in_mbs  = "256"
 }
 
