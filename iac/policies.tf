0136b3682bda23fff220b915b7b7732eb21a5b4e
Lucas Jellema <lucasjellema@gmail.com>
2021-11-17 18:03:46 +0100

    correcting policies for build pipelines

--- iac/policies.tf
+++ iac/policies.tf
@@ -6,6 +6,12 @@ resource "oci_identity_dynamic_group" "devops_pipln_dg" {
   matching_rule  = "All {resource.type = 'devopsdeploypipeline', resource.compartment.id = '${var.compartment_ocid}'}"
 }
 
+resource "oci_identity_dynamic_group" "devops_buildpipeline_dg" {
+  compartment_id = var.tenancy_ocid
+  name           = "${var.app_name}-devops_buildpipeline_dg"
+  description    = "${var.app_name} DevOps Build Pipeline Dynamic Group"
+  matching_rule  = "All {resource.type = 'devopsbuildpipeline', resource.compartment.id = '${var.compartment_ocid}'}"
+}
 resource "oci_identity_dynamic_group" "cloudnative2021-devops_coderepositories" {
   compartment_id = var.tenancy_ocid
   name           = "cloudnative2021-devops_coderepositories-dg"
@@ -20,6 +26,24 @@ resource "oci_identity_policy" "devops_compartment_policies" {
   compartment_id = var.tenancy_ocid
   statements     = local.allow_devops_manage_compartment_statements
 }
+
+
+
+resource "oci_identity_policy" "devops_buildpipeline-dg_policies" {
+  depends_on  = [oci_identity_dynamic_group.devops_buildpipeline_dg]
+  name        = "${var.app_name}-devops_buildpipeline-dg_policies"
+  description = "${var.app_name} DevOps Build Pipeline Policies"
+  compartment_id = var.tenancy_ocid
+  ## Provide access to read deployment artifacts in the Deliver Artifacts stage, read DevOps code repository in the Build stage, and trigger deployment pipeline in the Trigger Deploy stage
+  ## To deliver artifacts, provide access to the Artifact Registry
+  ## To deliver artifacts, provide access to the Container Registry (OCIR):
+  statements     = ["Allow dynamic-group ${oci_identity_dynamic_group.devops_buildpipeline_dg.name} to manage devops-family in compartment id ${var.compartment_ocid}"
+  , "Allow dynamic-group ${oci_identity_dynamic_group.devops_buildpipeline_dg.name} to manage generic-artifacts in compartment id ${var.compartment_ocid}"
+  , "Allow dynamic-group ${oci_identity_dynamic_group.devops_buildpipeline_dg.name} to manage repos in compartment id ${var.compartment_ocid}"
+  ]
+}
+
+
 
 locals {
   devops_pipln_dg = oci_identity_dynamic_group.devops_pipln_dg.name
