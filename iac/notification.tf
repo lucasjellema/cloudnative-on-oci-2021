3c3249d269c8380698dd7b2fe405c94a7329c5ae
Lucas Jellema <lucasjellema@gmail.com>
2021-11-25 15:29:22 +0100

    ; adding email subscription to topic

--- iac/notification.tf
+++ iac/notification.tf
@@ -3,7 +3,7 @@ resource "oci_ons_notification_topic" "cloudnative2021_twitter_report_alerts_top
   name           = "TWITTER_REPORT_ALERT"
 }
 
-resource oci_events_rule export_TWITTER_REPORT_EVENTS {
+resource oci_events_rule bucket_object_to_topic_notification_TWITTER_REPORT_EVENTS {
 depends_on     = [oci_ons_notification_topic.cloudnative2021_twitter_report_alerts_topic, oci_objectstorage_bucket.twitter_reports_bucket]
   actions {
     actions {
@@ -20,7 +20,8 @@ depends_on     = [oci_ons_notification_topic.cloudnative2021_twitter_report_aler
 }
 
 
-resource oci_ons_subscription export_subscription_1 {
+resource oci_ons_subscription tweet_report_digester_subscription_on_topic {
+  depends_on     = [ oci_ons_notification_topic.cloudnative2021_twitter_report_alerts_topic, oci_functions_function.tweet_report_digester_fn]
   compartment_id = var.compartment_ocid
   delivery_policy = "{\"backoffRetryPolicy\":{\"maxRetryDuration\":7200000,\"policyType\":\"EXPONENTIAL\"}}"
   endpoint        = "${oci_functions_function.tweet_report_digester_fn.id}"
@@ -28,4 +29,12 @@ resource oci_ons_subscription export_subscription_1 {
   }
   protocol = "ORACLE_FUNCTIONS"
   topic_id = oci_ons_notification_topic.cloudnative2021_twitter_report_alerts_topic.id
+}
+
+resource oci_ons_subscription emailsubscription_subscription_on_topic {
+  compartment_id = var.compartment_ocid
+  delivery_policy = "{\"backoffRetryPolicy\":{\"maxRetryDuration\":7200000,\"policyType\":\"EXPONENTIAL\"}}"
+  endpoint        = var.email_address_to_end_notification_of_tweetreport_to
+  protocol = "EMAIL"
+  topic_id = oci_ons_notification_topic.export_TWITTER_REPORT_ALERT.id
 }
